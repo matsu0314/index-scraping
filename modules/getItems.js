1341968a2b0bc3e0f6d411645d1fb721fd860a65
matsu <donguri47@gmail.com>
2022-07-24 19:43:59 +0900

    カスタムヘッダー修正

--- modules/getItems.js
+++ modules/getItems.js
@@ -16,8 +16,12 @@ module.exports = async (inputKeyword, res) => {
       headers: {
           referer: 'https://www.rakuten.co.jp/',
           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-      }
+      },
+      data: {}
     });
+
+
+
     console.log('response：' + baseURL + encodeKeyword);
 
     const htmlParser = response.data;
